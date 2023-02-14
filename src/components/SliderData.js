5dcc0e010d997735bc9e6baadcec0b504783d460
MotivatedUser <carbfixer@gmail.com>
2023-02-14 06:07:22 -0800

    Added alt info for sliders and made sliders responsive

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,14 +1,18 @@
 export const SliderData = [
     {
-        image: 'https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60'
+        image: 'https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60',
+        alt: "Computers on showing code"
     },
     {
-        image: 'https://images.unsplash.com/photo-1598015132635-131afe3ba07f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHRlYW0lMjB3b3JrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60'
+        image: 'https://images.unsplash.com/photo-1598015132635-131afe3ba07f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHRlYW0lMjB3b3JrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60',
+        alt: 'Teamwork spelled in scrable tiles'
     },
     {
-        image: 'https://images.unsplash.com/photo-1512223886638-d2914abf5df3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aW5zcGlyYXRpb25hbCUyMHF1b3RlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60'
+        image: 'https://images.unsplash.com/photo-1512223886638-d2914abf5df3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aW5zcGlyYXRpb25hbCUyMHF1b3RlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60',
+        alt: 'Good vibes messege board - Live Fully'
     },
     {
-        image: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhhcmQlMjB3b3JrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60'
+        image: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhhcmQlMjB3b3JrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60',
+        alt: 'Do something great in neon lights'
     }
 ]
